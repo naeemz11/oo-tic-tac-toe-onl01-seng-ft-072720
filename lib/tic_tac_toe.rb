cc9fd0e15b059df189f68d95de723cef5ffe43f6
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-07 02:06:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,7 +4,16 @@ class TicTacToe
     @board= board || Array.new(9, " ")
   end 
   
-  def WIN_COMBINATIONS
+ WIN_COMBINATIONS = [
+    [0,1,2], # Top row
+    [3,4,5],  # Middle row
+    [6,7,8],  # Bottom row
+    [0,3,6],  # Left col
+    [1,4,7],  # Middle col
+    [2,5,8],  # Right col
+    [0,4,8],  # Diagnol 1
+    [2,4,6]  # Diagnol 2
+  ]
   
   
 end 
